c2f08ac069202cc9d3741ec6c1bce9fa7714e9d6
sonia <soniarshin@gmail.com>
2026-01-14 00:20:45 -0800

    Revert "intake pivot (incomplete)"
    This reverts commit 37a9d540dffd37821051793403937ddc1c8c8b78.

--- src/main/java/frc/robot/subsystems/intake/intakePivot/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/intake/intakePivot/IntakePivot.java
@@ -1,31 +1,5 @@
 package frc.robot.subsystems.intake.intakePivot;
 
-import frc.robot.lib.generic_subsystems.superstructure.GenericSuperstructure;
-
-public class IntakePivot extends GenericSuperstructure<IntakePivot.IntakePivotTarget> {
-    public enum IntakePivotTarget implements GenericSuperstructure.PositionTarget {
-    INTAKE(-67), // change values
-    STOW(67);
-
-    private double position;
-    private static final double EPSILON = IntakePivotConstants.POSITION_TARGET_EPSILON;
-
-    private IntakePivotTarget(double position) {
-      this.position = position;
-    }
-
-    public double getPosition() {
-      return position;
-    }
-
-    @Override
-    public double getEpsilon() {
-      return EPSILON;
-    }
-
-  }
-  
-  public IntakePivot(IntakePivotIO io) {
-    super("Intake Pivot", io);
-  }
+public class IntakePivot {
+    
 }
