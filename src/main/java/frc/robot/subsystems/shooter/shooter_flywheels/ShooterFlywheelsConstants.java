60dea2c447acb967c953f80756861a166060b53d
alisa <alisal.singyee@gmail.com>
2026-01-15 17:55:17 -0800

    constants

---
+++ src/main/java/frc/robot/subsystems/shooter/shooter_flywheels/ShooterFlywheelsConstants.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.shooter.shooter_flywheels;
+
+import frc.robot.Constants;
+
+public class ShooterFlywheelsConstants {
+    public static final int ID;
+    public static final int CURRENT_LIMIT_AMPS;
+    public static final boolean INVERTED;
+    public static final boolean BRAKE;
+    public static final double REDUCTION;
+
+    //idk if we need the stuff below; from 2025 spring
+    public static final double MOI;
+    public static final double FILTERED_SUPPLY_CURRENT_THRESHOLD_INTAKING;
+}
