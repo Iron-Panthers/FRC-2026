481619452d0b3662e73928a4c216f534a58c69bf
alisa <alisal.singyee@gmail.com>
2026-01-15 18:14:53 -0800

    updated constants so it builds

---
+++ src/main/java/frc/robot/subsystems/shooter/shooter_flywheels/ShooterFlywheels.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.shooter.shooter_flywheels;
+
+import frc.robot.lib.generic_subsystems.rollers.*;
+
+public class ShooterFlywheels extends GenericRollers<ShooterFlywheels.Target>{
+    public enum Target implements GenericRollers.VoltageTarget {
+        //probably need to change; from sprint 2025
+        IDLE(0),
+        INTAKE(12),
+        HOLD(0.25),
+        EJECT(-3),
+        PASS(1);
+
+        private double volts;
+
+        private Target(double volts) {
+            this.volts = volts;
+        }
+
+        public double getVolts() {
+            return volts;
+        }
+    }
+
+    public ShooterFlywheels(ShooterFlywheelsIO io) {
+        super("Shooter Flywheels", io);
+    }
+}
--- src/main/java/frc/robot/subsystems/shooter/shooter_flywheels/ShooterFlywheelsConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/shooter_flywheels/ShooterFlywheelsConstants.java
@@ -3,13 +3,36 @@ package frc.robot.subsystems.shooter.shooter_flywheels;
 import frc.robot.Constants;
 
 public class ShooterFlywheelsConstants {
-    public static final int ID;
-    public static final int CURRENT_LIMIT_AMPS;
-    public static final boolean INVERTED;
-    public static final boolean BRAKE;
-    public static final double REDUCTION;
+    public static final int ID =
+        switch (Constants.getRobotType()){
+            case COMP -> 40;
+            case SIM -> 32;
+            default -> 0;
+        };
+    public static final int CURRENT_LIMIT_AMPS =
+        switch (Constants.getRobotType()) {
+            case COMP -> 40;
+            case SIM -> 40;
+            default -> 40;
+        };
+    public static final boolean INVERTED =
+        switch (Constants.getRobotType()) {
+            case COMP -> true;
+            case SIM -> true;
+            default -> true;
+        };
+    public static final boolean BRAKE =
+        switch (Constants.getRobotType()) {
+            default -> true;
+        };
+    public static final double REDUCTION =
+        switch (Constants.getRobotType()) {
+            case COMP -> 5;
+            case SIM -> 5;
+            default ->1;
+        };
 
     //idk if we need the stuff below; from 2025 spring
-    public static final double MOI;
-    public static final double FILTERED_SUPPLY_CURRENT_THRESHOLD_INTAKING;
+    public static final double MOI = 0.000105;
+    public static final double FILTERED_SUPPLY_CURRENT_THRESHOLD_INTAKING = 23;
 }
