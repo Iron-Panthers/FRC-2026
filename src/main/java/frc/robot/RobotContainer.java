9a71ecd0c555cf0956ea01504230a39c6e02df68
May-Bee-Lunar <maybeelunar0@gmail.com>
2026-01-19 16:07:58 -0800

    Added a match timer with extra info!
    In Elastic, and in Add Widget, go to AdvantageKit -> RealOutputs and I put all of the stuff in the bottom. It has info on what phase of the game is happening, which hubs are active, and of course the match timer!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import frc.robot.commands.VibrateHIDCommand;
 import frc.robot.subsystems.canWatchdog.CANWatchdog;
 import frc.robot.subsystems.canWatchdog.CANWatchdogIO;
 import frc.robot.subsystems.canWatchdog.CANWatchdogIOComp;
+import frc.robot.subsystems.elastic_updater.ElasticUpdater;
 import frc.robot.subsystems.rgb.RGB;
 import frc.robot.subsystems.rgb.RGBIO;
 import frc.robot.subsystems.rgb.RGBIOCANdle;
@@ -50,6 +51,8 @@ public class RobotContainer {
   private RobotState robotState = RobotState.getInstance();
 
   private ElasticSetpoints elasticSetpoints = ElasticSetpoints.getInstance();
+
+  private ElasticUpdater matchTimerUpdater = new ElasticUpdater();
 
   // private SendableChooser<Command> autoChooser;
   private LoggedDashboardChooser<Command> autoChooser;
