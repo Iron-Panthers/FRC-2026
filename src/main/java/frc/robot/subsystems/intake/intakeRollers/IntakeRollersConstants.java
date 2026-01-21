439a8655a3bf5a899c4a6d8267dbe149b88a7853
fivepuppies2813 <stanleyhsu73@gmail.com>
2026-01-20 17:10:36 -0800

    this is + the constants for liam

---
+++ src/main/java/frc/robot/subsystems/intake/intakeRollers/IntakeRollersConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.intake.intakeRollers;
+
+public class IntakeRollersConstants {
+    public static final int ID = 1;
+    public static final int CURRENT_LIMIT_AMPS = 1;
+    public static final boolean INVERTED = true;
+    public static final boolean BRAKE = true;
+    public static final int REDUCTION = 1;
+}
