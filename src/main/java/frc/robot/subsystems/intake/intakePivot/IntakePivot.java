016cb7ee66af78657481b830679ecbeee2115af7
stanley hsu <stanleyhsu73@gmail.com>
2026-01-24 12:11:35 -0800

    intake rollers completed maybe
    liam or someone review it please

--- src/main/java/frc/robot/subsystems/intake/intakePivot/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/intake/intakePivot/IntakePivot.java
@@ -1,7 +1,14 @@
 package frc.robot.subsystems.intake.intakePivot;
 
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import frc.robot.lib.generic_subsystems.superstructure.GenericSuperstructure;
 import frc.robot.subsystems.intake.intakePivot.IntakePivotConstants;
+import frc.robot.utility.LoggableMechanism3d;
 
 public class IntakePivot extends GenericSuperstructure<IntakePivot.IntakePivotTarget>{
   public enum IntakePivotTarget implements GenericSuperstructure.PositionTarget {
