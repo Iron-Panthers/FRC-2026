188488b742d1b86866b1b727320174840107ef06
alisa <alisal.singyee@gmail.com>
2026-02-03 19:12:03 -0800

    all deprecated dependencies in vision shouldddd be fixed

--- src/main/java/frc/robot/subsystems/vision/VisionIOPhotonvision.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOPhotonvision.java
@@ -20,7 +20,6 @@ public class VisionIOPhotonvision implements VisionIO {
     estimator =
         new PhotonPoseEstimator(
             VisionConstants.APRIL_TAG_FIELD_LAYOUT,
-            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
             VisionConstants.CAMERA_TRANSFORM[index]);
   }
 
@@ -36,7 +35,12 @@ public class VisionIOPhotonvision implements VisionIO {
       PhotonPipelineResult frame = results.get(frameIndex);
       if (!frame.hasTargets()) continue;
 
-      Optional<EstimatedRobotPose> optEstimation = estimator.update(frame);
+      Optional<EstimatedRobotPose> optEstimation;
+      if (frame.multitagResult.isEmpty()){
+        optEstimation = estimator.estimateCoprocMultiTagPose(frame);
+      } else {
+        optEstimation = estimator.estimateLowestAmbiguityPose(frame);
+      };
       if (optEstimation.isEmpty()) continue;
       EstimatedRobotPose estimation = optEstimation.get();
 
