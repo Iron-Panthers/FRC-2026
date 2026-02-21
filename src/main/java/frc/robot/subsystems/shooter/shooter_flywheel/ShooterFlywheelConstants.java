44a99577c869833c98a130b8c177b06889e404d1
Nora <nizcat5@gmail.com>
2026-02-21 14:16:47 -0800

    reducing amps, was creating brownouts

--- src/main/java/frc/robot/subsystems/shooter/shooter_flywheel/ShooterFlywheelConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/shooter_flywheel/ShooterFlywheelConstants.java
@@ -28,7 +28,7 @@ public class ShooterFlywheelConstants {
 
     public static final int CURRENT_LIMIT_AMPS =
         switch (Constants.getRobotType()) {
-            case COMP -> 1000;
+            case COMP -> 40;
             case SIM -> 40;
             default -> 40;
         };
