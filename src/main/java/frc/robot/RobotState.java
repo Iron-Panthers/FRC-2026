39456940eabfc8480e135b84140ed9ecd6b93ce3
Bruce Peters <brucebpeters12@gmail.com>
2026-02-24 20:13:13 -0800

    tuned constants

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -252,13 +252,13 @@ public class RobotState {
       this.shooterTable.clear();
       switch (Constants.getRobotType()) {
         case SIM -> {
-            this.shooterTable.put(1.3, new HoodParams(88, 1.621));
-        this.shooterTable.put(2.0, new HoodParams(84.5, 1.621));
-        this.shooterTable.put(2.5, new HoodParams(82, 1.601));
-        this.shooterTable.put(3.0, new HoodParams(79.5, 1.602));
-        this.shooterTable.put(3.5, new HoodParams(77.5, 1.581));
-        this.shooterTable.put(4.0, new HoodParams(75.5, 1.561));
-        this.shooterTable.put(4.5, new HoodParams(74, 1.561));
+            this.shooterTable.put(1.3, new HoodParams(84, 1.621));
+        this.shooterTable.put(2.0, new HoodParams(80.5, 1.621));
+        this.shooterTable.put(2.5, new HoodParams(78, 1.601));
+        this.shooterTable.put(3.0, new HoodParams(75.5, 1.602));
+        this.shooterTable.put(3.5, new HoodParams(73.5, 1.581));
+        this.shooterTable.put(4.0, new HoodParams(71.5, 1.561));
+        this.shooterTable.put(4.5, new HoodParams(70, 1.561));
         }
         default -> {
       this.shooterTable.put(2.0, new HoodParams(tempShooterAngle.get(), 1.621));
