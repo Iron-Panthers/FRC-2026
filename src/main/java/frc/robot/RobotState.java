7d4d4d308b35855ffce28d809627cc8b75780760
Bruce Peters <brucebpeters12@gmail.com>
2026-02-24 19:53:47 -0800

    fixed strange diff

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -42,6 +42,7 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -400,20 +401,9 @@ public class RobotState {
   public static boolean isAllianceRed() {
     //where true is red and false is blue
     var alliance = DriverStation.getAlliance();
-    if (alliance.isPresent()) {
+    if (RobotBase.isReal()) {
       return alliance.get() == DriverStation.Alliance.Red;
     }
     return false;
-  }
-
-  public Pose2d getClimbTarget(){
-    Pose2d climbLeftPose = DriveConstants.CLIMB_LEFT_POSE;
-    Pose2d climbRightPose = DriveConstants.CLIMB_RIGHT_POSE;
-    if (climbRightPose.getTranslation().getDistance(estimatedPose.getTranslation()) <
-        climbLeftPose.getTranslation().getDistance(estimatedPose.getTranslation())){
-      return climbLeftPose;
-    }else{
-      return climbRightPose;
-    }
   }
 }
