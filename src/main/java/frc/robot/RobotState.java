adc2984f52e1dbee9c656a12766d6f193164277e
Bruce Peters <brucebpeters12@gmail.com>
2026-02-24 20:04:50 -0800

    added shooter prediction constants back for sim

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -250,7 +250,22 @@ public class RobotState {
 
     public void initializeShooterTable(){
       this.shooterTable.clear();
+      switch (Constants.getRobotType()) {
+        case SIM -> {
+            this.shooterTable.put(1.3, new HoodParams(88, 1.621));
+        this.shooterTable.put(2.0, new HoodParams(84.5, 1.621));
+        this.shooterTable.put(2.5, new HoodParams(82, 1.601));
+        this.shooterTable.put(3.0, new HoodParams(79.5, 1.602));
+        this.shooterTable.put(3.5, new HoodParams(77.5, 1.581));
+        this.shooterTable.put(4.0, new HoodParams(75.5, 1.561));
+        this.shooterTable.put(4.5, new HoodParams(74, 1.561));
+        }
+        default -> {
       this.shooterTable.put(2.0, new HoodParams(tempShooterAngle.get(), 1.621));
+        }
+      }
+
+
     }
 
     public TargetShootingState calculateTargetShootingState(){
