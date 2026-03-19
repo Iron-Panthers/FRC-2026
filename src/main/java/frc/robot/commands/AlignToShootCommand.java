54f0b4b919f015e74e683782502209ecc076f1aa
Bruce Peters <brucebpeters12@gmail.com>
2026-03-19 16:41:18 -0700

    code change 2

--- src/main/java/frc/robot/commands/AlignToShootCommand.java
+++ src/main/java/frc/robot/commands/AlignToShootCommand.java
@@ -1,28 +1,14 @@
 package frc.robot.commands;
 
-
-import org.littletonrobotics.junction.Logger;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.RobotState;
 import frc.robot.subsystems.shooter.ShooterController;
-import frc.robot.subsystems.shooter.ShooterController.ShooterState;
 import frc.robot.subsystems.swerve.Drive;
-import frc.robot.subsystems.swerve.DriveConstants;
 
 /**
- * Aligns the robot to the shooting pose and spins up the shooter once
- * close enough. Intended to be used with whileTrue.
+ * Aligns the robot to the shooting pose and spins up the shooter once close enough. Intended to be
+ * used with whileTrue.
  */
-public class AlignToShootCommand extends Command{
+public class AlignToShootCommand extends Command {
   private Drive swerve;
   private ShooterController shooterController;
 
