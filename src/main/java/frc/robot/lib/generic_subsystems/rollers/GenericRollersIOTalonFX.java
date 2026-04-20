5f1cd6566b137799963ae59eacc0da1136520f97
Taku <takuwonoe@gmail.com>
2026-04-20 16:16:34 -0700

    Fixed a weird bug when merging

--- src/main/java/frc/robot/lib/generic_subsystems/rollers/GenericRollersIOTalonFX.java
+++ src/main/java/frc/robot/lib/generic_subsystems/rollers/GenericRollersIOTalonFX.java
@@ -59,11 +59,12 @@ public abstract class GenericRollersIOTalonFX implements GenericRollersIO {
       followerTalon.getConfigurator().apply(config);
       followerMotors.add(followerTalon);
     }
-
+    
     position = talon.getPosition();
     velocity = talon.getVelocity();
     appliedVolts = talon.getMotorVoltage();
     supplyCurrent = talon.getSupplyCurrent();
+    statorCurrent = talon.getStatorCurrent();
     BaseStatusSignal.setUpdateFrequencyForAll(50, velocity, appliedVolts, supplyCurrent);
 
     if (followerMotors.size() == 0) {
